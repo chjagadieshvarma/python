9431c2845f9a9f86d20134097986898cb17bc6fd
chjagadieshvarma <ch.jagadieshvarma23@gmail.com>
2026-02-13 11:01:48 +0530

    Add files via upload
    Operations and conversions

---
+++ day 2 - Operations and Conversions.py
@@ -0,0 +1,37 @@
+"""
+# read input numbers from users
+num = input()
+print(type(num)) # checking the type of number
+print(num)"""
+
+"""
+# implict type conversion
+
+num1 = int(input())
+val1 = float(input())
+print(type(num1), type(val1)) #checking the type of value
+res = num1+val1
+print(res)
+"""
+
+"""
+num1 = input() #read string type number
+print(type(num1)) # checking the type of number
+res = num1 + 10
+print(res)
+"""
+
+"""
+# TO overcome the above problem by using the Expliict Type conversion
+num1=input()
+print(type(num1))
+res=int(num1)+10
+print(res)
+"""
+
+#Convert intrzer to float, string, Boolean
+
+num1= 12
+print(float(num1))
+print(str(num1))
+print(bool(num1))
