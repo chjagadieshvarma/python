9aa0a7f7af54518bda80ea7473ecdfba053f059f
chjagadieshvarma <ch.jagadieshvarma23@gmail.com>
2026-02-14 21:24:44 +0530

    Add files via upload
    Day-3 operators and conditional statements

---
+++ Day3 Operators amd conditional Statements.py
@@ -0,0 +1,95 @@
+"""
+# check if number is zero
+num = int(input())
+if num == 0:
+    print("true the number is zero")
+print("program done")
+"""
+
+"""#check if number is zero or not
+num = int(input())
+if num == 0:
+    print("true the number is zero")
+else:
+    print("false the number is not zero")
+print("program done")
+"""
+
+"""
+num = int(input())
+if num != 0:
+    print("the number is not zero")
+else:
+    print("the number is zero")
+print("program done")
+
+"""
+
+"""
+#
+num = int(input())
+if num > 0 :
+    print("Number is greater than zero")
+elif num==0:
+    print("Number is zero")
+else:
+    print("Number is less than zero")
+
+"""
+"""
+# find bigger number among two numbers
+num1 = int(input())
+num2 = int(input())
+if num1>num2:
+    print(num1,"is greater than",num2)
+else:
+    print(num1,"is smaller than",num2)
+
+"""
+"""
+
+#find whether number is even or odd
+num1 = int(input())
+if num1 % 2 ==0:
+    print("the number is even number")
+else:
+    print("the number is odd number")
+
+"""
+
+"""
+#number is divisible by 5 or not
+
+num1 = int(input())
+if num1%5 ==0:
+    print("the number is divisible by 5")
+else:
+    print("the number is not divisible by 5")
+
+"""
+
+"""
+# number is divisible by 5 and 3 or not
+
+num1 = int(input())
+if num1%5==0 :
+    print("the number is divisible by 5")
+elif num1%3==0:
+    print("the number is divisible by 3")
+elif num1%5==0 and num1%3==0:
+    print("the number is divisible by both 5 and 3")
+else:
+    print("the number is not divisible by 5 and 3")
+
+"""
+
+#
+num1 = int(input())
+if num1%4==0 :
+    print("the number is divisible by 4")
+elif num1%6==0:
+    print("the number is divisible by 6")
+elif num1%4==0 and num1%6==0:
+    print("the number is divisible by both 4 and 6")
+else:
+    print("the number is not divisible by 4 and 6")
